8b98bd58a8ca3c5a3185de76ff162398a4249445
prakash <prakash.selvaraj@outlook.com>
2020-03-18 08:02:17 +0530

    -- unit test added for tag service

---
+++ tests/init.spec.js
@@ -0,0 +1,12 @@
+const mongoUnit = require('mongo-unit')
+ 
+mongoUnit.start().then(() => {
+  console.log('fake mongo is started: ', mongoUnit.getUrl())
+  process.env.DATABASE_URL = mongoUnit.getUrl() // this var process.env.DATABASE_URL = will keep link to fake mongo
+  run() // this line start mocha tests
+})
+ 
+after(() => {
+  console.log('stop')
+  return mongoUnit.stop()
+})
---
+++ tests/tag/tag.service.spec.js
@@ -0,0 +1,43 @@
+const { expect } = require("chai");
+const mongoUnit = require("mongo-unit");
+const tagUT = require("../../resources/tag/tag.service");
+var mongoose = require("mongoose");
+
+describe("tags", () => {
+  const tagData = {
+    tags: [
+      {
+        _id: "56d9bf92f9be48771d6fe5b1",
+        name: "test"
+      },
+      {
+        _id: "56d9bf92f9be48771d6fe5b2",
+        name: "John"
+      }
+    ]
+  };
+  before(() => mongoose.connect(process.env.DATABASE_URL));
+  beforeEach(() => mongoUnit.load(tagData));
+  afterEach(() => mongoUnit.drop());
+
+  it("should find all tags", () => {
+    return tagUT.getTrendingTags().then(tags => {
+      expect(tags.length).to.equal(2);
+      expect(tags[0].name).to.equal("test");
+    });
+  });
+
+  it("should create new tag", () => {
+    const newTagName = "NewTag";
+    return tagUT
+      .createTag({ name: newTagName })
+      .then(tag => {
+        expect(tag.name).to.equal(newTagName.toLowerCase());
+      })
+      .then(() => tagUT.getTrendingTags())
+      .then(tags => {
+        expect(tags.length).to.equal(3);
+        expect(tags[2].name).to.equal(newTagName.toLowerCase());
+      });
+  });
+});
